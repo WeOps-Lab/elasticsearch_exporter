cb51bf61fe4620388061dc55fc1cba9cd5d32824
zhongming Fan <zhongmingfan@canway.net>
2023-10-19 15:05:47 +0800

    dp: unprivileges

--- weops/pipe-tools/exporter/template/build.sh
+++ weops/pipe-tools/exporter/template/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for version in v5-6 v7-17 v8-10; do
+for version in v7-17 v8-10; do
     # standalone
     standalone_output_file="standalone_${version}.yaml"
     sed "s/{{VERSION}}/${version}/g;" standalone.tpl > ../standalone/${standalone_output_file}
 done
 
-for version in v6-8; do
+for version in v5-6 v6-8; do
     # standalone
     standalone_output_file="standalone_${version}.yaml"
-    sed "s/{{VERSION}}/${version}/g;" v6-8.tpl > ../standalone/${standalone_output_file}
+    sed "s/{{VERSION}}/${version}/g;" unprivileges.tpl > ../standalone/${standalone_output_file}
 done
